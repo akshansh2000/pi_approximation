2f5876c93b299c3a125a06481e37ecb7e5250f9e
Akshansh Bhanjana <akshansh2000@gmail.com>
2020-05-07 13:20:34 +0530

    added simulation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'dart:math' as math;
+
 main() {
   runApp(
     MaterialApp(
@@ -10,9 +12,16 @@ main() {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key key}) : super(key: key);
 
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+double pi = 0;
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(
@@ -28,49 +37,99 @@ class MyApp extends StatelessWidget {
       DeviceOrientation.portraitUp,
     ]);
 
-    return Scaffold(
-      backgroundColor: Colors.grey[900],
-      bottomNavigationBar: CustomBottomBar(),
-    );
-  }
-}
-
-class CustomBottomBar extends StatefulWidget {
-  CustomBottomBar({Key key}) : super(key: key);
-
-  @override
-  _CustomBottomBarState createState() => _CustomBottomBarState();
-}
-
-class _CustomBottomBarState extends State<CustomBottomBar> {
-  double pi = 0;
-
-  @override
-  Widget build(BuildContext context) {
     WidgetsBinding.instance
         .addPostFrameCallback((timeStamp) => setState(() {}));
 
-    return BottomAppBar(
-      elevation: 0,
-      color: Colors.white,
-      child: Padding(
-        padding: EdgeInsets.symmetric(vertical: 30),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Pi Value (approx)",
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            SizedBox(height: 10),
-            Text(
-              "$pi",
-              style: Theme.of(context).textTheme.headline5,
-            ),
-          ],
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      bottomNavigationBar: BottomAppBar(
+        elevation: 0,
+        color: Colors.white,
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 30),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Pi Value (approx)",
+                style: Theme.of(context).textTheme.headline4.copyWith(
+                      color: Colors.black,
+                    ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                "${pi.toStringAsFixed(6)}",
+                style: Theme.of(context).textTheme.headline5,
+              ),
+            ],
+          ),
         ),
+      ),
+      body: CustomPaint(
+        painter: SimulationPainter(),
+        child: Container(),
       ),
     );
   }
+}
+
+final coordinates = List<List<double>>();
+
+class SimulationPainter extends CustomPainter {
+  final randomGenerator = math.Random();
+  double x, y;
+  int insideCircle = 0, total = 0;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final radius = size.width * 9 / 20;
+
+    final paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 2
+      ..color = Colors.white;
+
+    canvas.translate(size.width / 2, size.height / 2);
+
+    canvas.drawCircle(Offset(0, 0), radius, paint);
+    canvas.drawRect(
+      Rect.fromCenter(
+        center: Offset(0, 0),
+        height: radius * 2,
+        width: radius * 2,
+      ),
+      paint,
+    );
+
+    paint.style = PaintingStyle.fill;
+    for (int i = 0; i < 90; i++) {
+      x = -radius + randomGenerator.nextDouble() * radius * 2;
+      y = -radius + randomGenerator.nextDouble() * radius * 2;
+
+      coordinates.add([x, y]);
+
+      ++total;
+      if (x * x + y * y < radius * radius) ++insideCircle;
+    }
+
+    coordinates.forEach((element) {
+      x = element[0];
+      y = element[1];
+
+      paint.color = (x * x + y * y > radius * radius)
+          ? Colors.redAccent
+          : Colors.greenAccent;
+
+      canvas.drawCircle(Offset(x, y), 0.3, paint);
+
+      pi = insideCircle / total * 4;
+    });
+  }
+
+  @override
+  bool shouldRepaint(SimulationPainter oldDelegate) => true;
+
+  @override
+  bool shouldRebuildSemantics(SimulationPainter oldDelegate) => false;
 }
